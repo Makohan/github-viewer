a6d2c7399f331fd444555f133d3218ce80a7b917
mkin <32333141+Makohan@users.noreply.github.com>
2023-06-29 09:09:45 +0900

    feat: add comments

--- src/routes/issues/[id]/+page.server.ts
+++ src/routes/issues/[id]/+page.server.ts
@@ -16,9 +16,19 @@ export async function load({ params }) {
 	console.log('-----');
 	console.log(res.data);
 
+	const res2 = await octokit.rest.issues.listComments({
+		owner: OWNER,
+		repo: REPO,
+		issue_number: Number(params.id)
+	});
+
+	console.log('-----');
+	console.log(res2.data);
+
 	if (res) {
 		return {
-			issue: res.data
+			issue: res.data,
+			comments: res2.data
 		};
 	}
 
--- src/routes/issues/[id]/+page.svelte
+++ src/routes/issues/[id]/+page.svelte
@@ -3,8 +3,21 @@
 
 	export let data;
 
-	const { issue } = data;
+	const { issue, comments } = data;
 </script>
 
 <h1>{issue.title}</h1>
-<p>{@html marked.parse(issue.body)}</p>
+
+<section>
+	<p>{issue.user.login} updated_at: {issue.updated_at}</p>
+	<p>{@html marked.parse(issue.body)}</p>
+</section>
+---
+<section>
+	{#each comments as comment}
+		<article>
+			<p>{comment.user.login} updated_at: {comment.updated_at}</p>
+			<p>{@html marked.parse(comment.body)}</p>
+		</article>
+	{/each}
+</section>
