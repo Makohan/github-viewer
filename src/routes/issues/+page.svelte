fb96bd810a7e9c2add5c62428f44cad0f4d69dba
mkin <32333141+Makohan@users.noreply.github.com>
2023-06-30 10:06:38 +0900

    fix: style

--- src/routes/issues/+page.svelte
+++ src/routes/issues/+page.svelte
@@ -20,20 +20,20 @@
 </script>
 
 <h1
-	class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+	class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl"
 >
 	Issue List
 </h1>
 
 <div class="w-32">
-	<label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+	<label for="countries" class="block mb-2 text-sm font-medium text-gray-900"
 		>Select an option</label
 	>
 	<select
 		bind:value={selectedLabel}
 		on:change={filter}
 		id="countries"
-		class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+		class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 	>
 		<option value="" />
 		{#each labels as label}
@@ -44,8 +44,8 @@
 
 <div class="mt-4">
 	<div class="relative overflow-x-auto">
-		<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-			<thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+		<table class="w-full text-sm text-left text-gray-500">
+			<thead class="text-xs text-gray-700 bg-gray-50">
 				<tr>
 					<th scope="col" class="px-6 py-3"> Title </th>
 					<th scope="col" class="px-6 py-3"> Label </th>
@@ -55,10 +55,10 @@
 			</thead>
 			<tbody>
 				{#each issues as issue}
-					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+					<tr class="bg-white border-b">
 						<th
 							scope="row"
-							class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+							class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
 						>
 							<a href={`/issues/${issue.number}`}>{issue.title} </a>
 						</th>
