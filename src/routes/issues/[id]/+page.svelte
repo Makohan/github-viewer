38e73ccbb8b91a80496038128824ce4cefaee3f1
mkin <32333141+Makohan@users.noreply.github.com>
2023-07-02 03:24:55 +0900

    feat: style

--- src/routes/issues/[id]/+page.svelte
+++ src/routes/issues/[id]/+page.svelte
@@ -10,9 +10,9 @@
 	const daysAfterCreatedAt = dayjs(issue.created_at).diff(dayjs(), 'day');
 </script>
 
-<h1 class="font-bold text-xl">{issue.title}</h1>
+<h1 class="text-xl font-bold">{issue.title}</h1>
 <div>
-	<div class="flex items-center gap-x-2 border-b pb-2">
+	<div class="flex items-center pb-2 border-b gap-x-2">
 		<div class="text-sm text-white px-2 rounded-3xl {isOpen ? 'bg-green-500' : 'bg-purple-500'}">
 			{issue.state}
 		</div>
@@ -22,7 +22,7 @@
 		</div>
 	</div>
 	{#if issue.body}
-		<div class="pt-2 marked">{@html marked.parse(issue.body)}</div>
+		<div class="pt-2 issue">{@html marked.parse(issue.body)}</div>
 	{/if}
 </div>
 
@@ -31,13 +31,13 @@
 		<Spinner />
 	{:then comments}
 		{#each comments.data as comment}
-			<div class="border rounded-lg my-2">
-				<div class="flex gap-x-2 text-sm bg-gray-50 py-1 px-2">
+			<div class="my-2 border rounded-lg">
+				<div class="flex px-2 py-1 text-sm gap-x-2 bg-gray-50">
 					<div>{comment.user?.login}</div>
 					<div>{dayjs(comment.updated_at).format('YYYY-MM-DD hh:mm')}</div>
 				</div>
 				{#if comment.body}
-					<div class="px-2 marked">{@html marked.parse(comment.body)}</div>
+					<div class="px-2 issue">{@html marked.parse(comment.body)}</div>
 				{/if}
 			</div>
 		{/each}
@@ -47,22 +47,22 @@
 </div>
 
 <style>
-	:global(.marked) {
+	:global(.issue) {
 		@apply text-xs;
 	}
-	:global(.marked h1, .marked h2, .marked h3, .marked h4, .marked h5, .marked h6) {
+	:global(.issue h1, .issue h2, .issue h3, .issue h4, .issue h5, .issue h6) {
 		@apply font-bold;
 	}
-	:global(.marked h1) {
+	:global(.issue h1) {
 		@apply text-xl border-b;
 	}
-	:global(.marked h2) {
+	:global(.issue h2) {
 		@apply text-lg border-b;
 	}
-	:global(.marked p) {
+	:global(.issue p) {
 		@apply pb-2;
 	}
-	:global(.marked ul li) {
+	:global(.issue ul li) {
 		@apply list-disc ml-4;
 	}
 </style>
