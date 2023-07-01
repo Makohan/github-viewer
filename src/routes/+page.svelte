d9a98329f3870d24b94150b8db8730ad4dd39adb
mkin <32333141+Makohan@users.noreply.github.com>
2023-07-02 04:03:55 +0900

    feat: top page description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
-<ul>
-	<li><a href="/issues">リストへ</a></li>
-	<li><a href="/projects">プロジェクトへ（Orgのみ）</a></li>
-</ul>
+<h1 class="text-lg font-bold">GitHub Viewer is a simple service for viewing data on GitHub.</h1>
+
+<p class="mt-2">
+	This tool is designed for efficiently viewing Issues of a specific GitHub repository.<br />
+	By simply providing the information of the repository they wish to investigate as environment variables,
+	users can retrieve a list of Issues.<br />(Viewing of Projects is on the to-do list.)
+</p>
+
+<div class="mt-4">
+	<h2 class="font-bold">Link</h2>
+	<ul class="ml-4 text-blue-500">
+		<li><a href="/issues" class="hover:underline">to Issues</a></li>
+		<li><a href="/projects" class="hover:underline">to Projects（not yet）</a></li>
+	</ul>
+</div>
